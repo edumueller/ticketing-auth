b47852cdcf50bbdb4b27f123932c5f9a6ebb890e
edumueller <eduardo.nicastro@icloud.com>
2021-08-18 15:30:45 -0300

    refactor: implements req validation err class

--- src/routes/signup.ts
+++ src/routes/signup.ts
@@ -1,5 +1,8 @@
 import express, { Request, Response } from "express";
 import { body, validationResult } from "express-validator";
+import { ValidationError } from "express-validator";
+import { DatabaseConnectionError } from "../errors/database-connection-error";
+import { RequestValidationError } from "../errors/request-validation-error";
 
 const router = express.Router();
 
@@ -15,7 +18,7 @@ router.post(
   (req: Request, res: Response) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      throw new Error("Invalid email or password");
+      throw new RequestValidationError(errors.array());
     }
     const { email, password } = req.body;
     console.log("Creating a user...");
