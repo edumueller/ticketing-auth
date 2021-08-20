58638886303b926f4994de2889034665e486fbd8
edumueller <eduardo.nicastro@icloud.com>
2021-08-19 21:27:49 -0300

    refactor: moved logic to middleware

--- src/routes/signin.ts
+++ src/routes/signin.ts
@@ -1,9 +1,23 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import { body, validationResult } from "express-validator";
+import { BadRequestError } from "../errors/bad-request-error";
+import { validateRequest } from "../middlewares/request-validation";
+import { User } from "../models/user";
+import jwt from "jsonwebtoken";
 
 const router = express.Router();
 
-router.post("/api/users/signin", (req, res) => {
-  res.send("Hi there!");
-});
+router.post(
+  "/api/users/signin",
+  [
+    body("email").isEmail().withMessage("Email must be valid"),
+    body("password")
+      .trim()
+      .notEmpty()
+      .withMessage("You must suplly a password"),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {}
+);
 
 export { router as signinRouter };
