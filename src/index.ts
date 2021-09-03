6ec1ccf3c7c284bf149286cbdfaed1584efdf2db
edumueller <eduardo.nicastro@icloud.com>
2021-09-02 21:13:51 -0300

    add mongoose options

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,10 @@ const start = async () => {
   }
 
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log('Connected to MongoDb');
   } catch (err) {
     console.error(err);
